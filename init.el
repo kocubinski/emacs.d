bff9c1ec661bf96ff76d57c0c0a73c301d1d6ea1
Matt Kocubinski <mkocubinski@gmail.com>
2011-12-17 01:03:32 -0500

    Add AutoHotKey mode.

--- init.el
+++ init.el
@@ -98,3 +98,16 @@
 ;(require 'init-auto-complete)
 
 (load-file "~/.emacs.d/emacs-for-python/epy-init.el")
+(load-file "~/.emacs.d/ahk-mode.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ahk-syntax-directory "d:/programs/AutoHotkeyBasic/Extras/Editors/Syntax/"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
