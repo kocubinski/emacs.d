b19c1ff4ef8f28ff5063fe19c625340d3f47cacf
Matt Kocubinski <mkocubinski@gmail.com>
2012-05-22 15:48:05 -0400

    Update a lot of stuff.

--- init.el
+++ init.el
@@ -27,7 +27,12 @@
 
 (add-hook 'clojure-mode-hook
           (defun turn-on-clojure-test-mode ()
-            (clojure-test-mode 1)))
+            (clojure-test-mode 1)
+            (auto-complete-mode 1)))
+
+(add-hook 'inferior-lisp-mode-hook
+          (defun lisp-mode-hooks ()
+            (auto-complete-mode 1)))
 
 (add-hook 'slime-repl-mode-hook
           (defun clojure-mode-slime-font-lock ()
@@ -36,12 +41,12 @@
 
 ;(set-face-attribute 'default nil :font "Bitstream Vera Sans Mono-13")
 (if (eq system-type 'windows-nt) 
-    (set-face-attribute 'default nil :font "Consolas-14")
+    (set-face-attribute 'default nil :font "Consolas-13")
     (set-face-attribute 'default nil :font "Inconsolata-15"))
 
 
-
-(load-theme 'manoj-dark)
+;(load-theme 'manoj-dark)
+;(load-theme 'zenburn)
 
 (global-set-key (kbd "C-,") 'other-window)
 (global-set-key (kbd "C-<") 'previous-buffer)
@@ -79,8 +84,12 @@
       (file-name-directory (file-truename load-file-name))))
 
 (require 'init-evil)
-(require 'init-auto-complete)
+(require 'auto-complete)
 (require 'mwe-color-box)
+(require 'visual-basic-mode)
+
+(add-to-list 'load-path "~/.emacs.d/zencoding")
+(require 'zencoding-mode)
 
 (require 'moz)
 (autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
@@ -88,3 +97,23 @@
     (add-hook 'javascript-mode-hook 'javascript-custom-setup)
     (defun javascript-custom-setup ()
       (moz-minor-mode 1))
+
+;(load-file "~/.emacs.d/emacs-for-python/epy-init.el")
+
+(setq vc-handled-backends nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(custom-safe-themes (quote ("24d3369f5a236ea822bb4f786fe923d730dc070c" "965234e8069974a8b8c83e865e331e4f53ab9e74" default)))
+ '(fci-rule-color "#383838"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq inferior-lisp-program "script/repl")
