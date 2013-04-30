40649eba17ca094f8b19d2065ee6d5d3f89cac97
Matt Kocubinski <mkocubinski@gmail.com>
2013-04-30 17:03:36 -0400

    Use this version for js devel.  Add dos-mode.

--- init.el
+++ init.el
@@ -51,9 +51,8 @@
 (require 'mwe-color-box)
 (require 'nrepl)
 (require 'slime-js)
-(require 'slime-repl)
-(require 'slime-fancy)
-(slime-setup '(slime-repl slime-fancy slime-js))
+(require 'dos)
+(slime-setup '(slime-js))
 
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
@@ -211,3 +210,23 @@
 (defun clojure-md-color ()
   (interactive)
   (set (make-local-variable 'font-lock-string-face) 'clojure-md-string-face))
+
+
+;; After js2 has parsed a js file, we look for jslint globals decl
+;; comment ("/* global Fred, _, Harry */") and add any symbols to a
+;; buffer-local var of acceptable global vars Note that we also
+;; support the "symbol: true" way of specifying names via a hack
+;; (remove any ":true" to make it look like a plain decl, and any
+;; ':false' are left behind so they'll effectively be ignored as you
+;; can;t have a symbol called "someName:false"
+(add-hook 'js2-post-parse-callbacks
+          (lambda () (when (> (buffer-size) 0)
+                  (let ((btext (replace-regexp-in-string
+                                ": *true" " "
+                                (replace-regexp-in-string
+                                 "[\n\t ]+" " "
+                                 (buffer-substring-no-properties 1 (buffer-size))
+                                 t t))))
+                    (mapc (apply-partially 'add-to-list 'js2-additional-externs)
+                          (split-string (if (string-match "/\\* *global *\\(.*?\\) *\\*/" btext)
+                                            (match-string-no-properties 1 btext) "") " *, *" t))))))
