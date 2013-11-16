64761fcccf0c2f16f23377050205fbcdef6b9781
Matt Kocubinski <mkocubinski@gmail.com>
2013-11-16 16:52:38 -0500

    Update to CIDER, solarized.

--- init.el
+++ init.el
@@ -18,11 +18,11 @@
                       rainbow-delimiters
                       auto-complete
                       ac-slime
-                      nrepl
                       js2-mode
                       xml-rpc
                       org2blog
                       htmlize
+                      color-theme
                       zenburn-theme)
    "A list of packages to ensure are installed at launch.")
 
@@ -53,7 +53,6 @@
 (require 'init-evil)
 (require 'init-auto-complete)
 (require 'mwe-color-box)
-(require 'nrepl)
 (require 'slime-js)
 (require 'dos)
 (require 'htmlize)
@@ -107,11 +106,11 @@
             (save-buffer)
             (slime-compile-and-load-file)))
 
-;(set-face-attribute 'default nil :font "Bitstream Vera Sans Mono-13")
-(when (display-graphic-p)
-  (if (eq system-type 'windows-nt) 
-      (set-face-attribute 'default nil :font "Consolas-13")
-    (set-face-attribute 'default nil :font "Inconsolata-15")))
+(set-face-attribute 'default nil :font "Inconsolata for Powerline-12")
+;; (when (display-graphic-p)
+;;   (if (eq system-type 'windows-nt) 
+;;       (set-face-attribute 'default nil :font "Consolas-13")
+;;     (set-face-attribute 'default nil :font "Inconsolata-15")))
 
 
 (global-set-key (kbd "C-,") 'other-window)
@@ -172,7 +171,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("24d3369f5a236ea822bb4f786fe923d730dc070c" "a7e8dc00fc8043439a738a15e2f593b8e9b2492f" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "b7553781f4a831d5af6545f7a5967eb002c8daeee688c5cbf33bf27936ec18b3" "965234e8069974a8b8c83e865e331e4f53ab9e74" default)))
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "24d3369f5a236ea822bb4f786fe923d730dc070c" "a7e8dc00fc8043439a738a15e2f593b8e9b2492f" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "b7553781f4a831d5af6545f7a5967eb002c8daeee688c5cbf33bf27936ec18b3" "965234e8069974a8b8c83e865e331e4f53ab9e74" default)))
  '(ispell-dictionary "english")
  '(ispell-program-name "C:\\Program Files (x86)\\Aspell\\bin\\aspell.exe")
  '(org-agenda-files (quote ("c:/dev/org/totalpro.org" "c:/dev/org/designer.org"))))
@@ -182,7 +181,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(load-theme 'zenburn)
+;(load-theme 'zenburn)
+(color-theme-solarized-dark)
 
 (require 'auto-complete)
 ;;; Usage
@@ -209,10 +209,10 @@
 
 ;(require 'auto-save)
 
-(set-face-background 'mode-line "dark slate blue")
-(setq evil-normal-state-cursor '("SeaGreen4" box))
-(setq evil-insert-state-cursor '("SeaGreen3" bar))
-(setq evil-emacs-state-cursor '("red" box))
+;(set-face-background 'mode-line "dark slate blue")
+;(setq evil-normal-state-cursor '("SeaGreen4" box))
+;(setq evil-insert-state-cursor '("SeaGreen3" bar))
+;(setq evil-emacs-state-cursor '("red" box))
 
 ;; I manually set this when editing clojure "markdown-ish" buffers,
 ;; i.e. those used for misaki.
